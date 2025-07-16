d662a05f5c577352744cd576afe6071ac2ca6e71
premchandellendula <premcellendula@gmail.com>
2025-07-16 16:07:33 +0530

    feat(auth): add auth routes and related schema

--- apps/http-server/src/routes/room/room.ts
+++ apps/http-server/src/routes/room/room.ts
@@ -1,9 +1,35 @@
 import express, { Router } from 'express'
 const router: Router = express.Router()
-// import { roomBody } from '@repo/common/types'
+import { roomBody } from '@repo/common/types'
+import { authMiddleware } from '../../middlewares/authMiddleware';
+import { prisma } from '@repo/db';
 
-router.post('/create-room',  async (req, res) => {
+router.post('/create-room', authMiddleware,  async (req, res) => {
+    const response = roomBody.safeParse(req.body);
 
+    if(!response.success){
+        res.status(400).json({
+            message: "Incorrect inputs"
+        })
+        return;
+    }
+
+    const { name } = response.data;
+
+    try {
+        const room = await prisma.room.create({
+            data: {
+                name,
+                adminId: req.userId,
+                code: "123213"
+            }
+        })
+    }catch(err) {
+        res.status(500).json({
+            message: "Error creating a room",
+            error: err instanceof Error ? err.message : "Unknown error"
+        })
+    }
 })
 
 export default router;
